1c2bea5825f825cbf82ad8001ded20f1d72da10a
Kassy729 <79403903+Kassy729@users.noreply.github.com>
2021-06-15 22:53:46 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,55 +1,78 @@
 const express = require('express');
-const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const dotenv = require('dotenv');
-const passport = require('passport');  //passport모듈 임포트
+const morgan = require('morgan');
 const path = require('path');
-const bodyParser = require('body-parser');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+const passport = require('passport');
 
 dotenv.config();
+const pageRouter = require('./routes/page');
+const authRouter = require('./routes/auth');
+const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
+const { sequelize } = require('./models');
+const passportConfig = require('./passport');
+const introduceRouter = require('./routes/introduce');
+const localRouter = require('./routes/local');
+
 const app = express();
-app.set('port', process.env.PORT || 3000);
+passportConfig(); // 패스포트 설정
+app.set('port', process.env.PORT || 8001);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
+});
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
-
-app.use(morgan('combined'));
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(bodyParser.raw());
-app.use(bodyParser.text());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
-    resave: false,
-    saveUninitialized: false, 
-    secret: process.env.COOKIE_SECRET,
-    cookie:{
-        httpOnly: true,
-        secure: false,
-    },
-    name: 'session-cookie',
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use((req,res,next) => {
-    console.log('모든 요청에 다 실행됩니다.');
-    next();
+app.use('/', pageRouter);
+app.use('/auth', authRouter);
+app.use('/post', postRouter);
+app.use('/user', userRouter);
+app.use('/introduce', introduceRouter);
+app.use('/local', localRouter);
+
+
+
+app.use((req, res, next) => {
+  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 });
 
-app.get('/', (req,res,next) => {
-    console.log('GET / 요청에서만 실행됩니다.');
-    next();
-}, (req, res) => {
-    throw new Error('에러는 에러 처리 미들웨어로 갑니다.')
-});
-
-app.use((err,req,res,next) => {
-    console.error(err);
-    res.status(500).send(err.message);
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 app.listen(app.get('port'), () => {
-    console.log(app.get('port'), '번 포트에서 대기 중');
-    console.log(bodyParser);
-    console.log(cookieParser);
+  console.log(app.get('port'), '번 포트에서 대기중');
 });
-
